626c5bf18dd8a80a381426bee492cf462b27afc7
nime42 <nils.meinhard@teliacompany.com>
2022-01-08 17:49:45 +0100

    running https or https is configurable

--- app/server.js
+++ app/server.js
@@ -7,10 +7,11 @@ var app = express();
 
 var config=require('../resources/config.js');
 
-
-var privateKey  = fs.readFileSync(config.certs.privateKey, 'utf8');
-var certificate = fs.readFileSync(config.certs.certificate, 'utf8');
-var ca = fs.readFileSync(config.certs.ca, 'utf8');
+if(config.app.https) {
+    var privateKey  = fs.readFileSync(config.certs.privateKey, 'utf8');
+    var certificate = fs.readFileSync(config.certs.certificate, 'utf8');
+    var ca = fs.readFileSync(config.certs.ca, 'utf8');
+}
 
 var credentials = {key: privateKey, cert: certificate,ca:ca};
 var express = require('express');
@@ -23,12 +24,14 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public'));
 
 
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(config.app.http,() => console.log('App listening at http://localhost:'+config.app.http));
-httpsServer.listen(config.app.https,() => console.log('App listening at https://localhost:'+config.app.https));
+if(config.app.http) {
+    var httpServer = http.createServer(app);
+    httpServer.listen(config.app.http,() => console.log('App listening at http://localhost:'+config.app.http));
+}
+if(config.app.https) {
+    var httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(config.app.https,() => console.log('App listening at https://localhost:'+config.app.https));
+}
 
 var db=require('./db-functions.js');
 
