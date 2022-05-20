e13cdf9c31d8743a64546f113a4c27a68c8370f5
nime42 <nilsmeinhard@gmail.com>
2022-05-20 19:26:49 +0200

    misc changes

--- resources/config.js
+++ resources/config.js
@@ -2,32 +2,39 @@ module.exports = {
 
     app: {
         http:8080,
-        https_do_not_use_for_the_moment:8443
+        https_do_not_use:8443
     },
 
     certs: {
         privateKey:"./resources/private.key",
         certificate:"./resources/certificate.crt",
         ca:"./resources/ca_bundle.crt"
+
     },
 
     grocy:{
-        url:"https://grocytest.ristetrug.dk",
-        apiKey:"??"
-      },
+            url:"https://grocytest.ristetrug.dk",
+            apiKey:"?????????"
+          },
 
     userDB:"./resources/users.db",
 
 
-    mail: {
+    mailX: {
         service: "gmail",
         user: "tipsy.nu@gmail.com",
-        passwd: "??",        
+        passwd: "???????",
         port:25
+    },
+    mail: {
+        host: "smtp.gigahost.dk",
+        user: "bon@ristetrug.dk",
+        passwd: "?????",
+        port:587
     },
 
     forgotPasswordMailTemplate:{
-        from:"tipsy@gmail.com",
+        from:"bon@ristetrug.dk",
         subject:"Återställ lösenord",
         body:`
         Hej
