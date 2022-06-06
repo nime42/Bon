aed71ee383978028c3bb94a74f8d8114f46d0eb5
nime42 <nilsmeinhard@gmail.com>
2022-06-06 19:35:23 +0200

    Fixed a bug in consume

--- app/grocy-functions.js
+++ app/grocy-functions.js
@@ -1,6 +1,14 @@
 const config = require('../resources/config.js');
 const fetch = require('node-fetch');
 
+/**
+ * Get all recipies from Grocy DB
+ * @param {*} callback - function(status,products)
+ *                          @param status {boolean} true if the call to grocy succeed.
+ *                          @param recepies {[Obj]} array with recepyinfo (see function parseRecipes below)
+ 
+ * 
+ */
 function getAllRecipes(callback = console.log) {
     let httpReq = config.grocy.url + "/api/objects/recipes?GROCY-API-KEY=" + config.grocy.apiKey;
 
@@ -14,6 +22,13 @@ function getAllRecipes(callback = console.log) {
         );
 }
 
+/**
+ * Get all products that belongs to a recipe.
+ * @param {*} recipe_external_id 
+ * @param {*} callback - function(status,products)
+ *                          @param status {boolean} true if the call to grocy succeed.
+ *                          @param products {[{product_id,amount}]} array with productid and the amount of the product needed for the recipe
+ */
 function getProductIds(recipe_external_id, callback = console.log) {
     let httpReq = config.grocy.url + "/api/objects/recipes_pos?query[]=recipe_id=" + recipe_external_id + "&GROCY-API-KEY=" + config.grocy.apiKey;
     fetch(httpReq)
@@ -28,6 +43,13 @@ function getProductIds(recipe_external_id, callback = console.log) {
         );
 }
 
+/**
+ * Get all sub recipies for the recipe.
+ * @param {*} recipe_external_id 
+ * @param {*} callback - function(status,products)
+ *                          @param status {boolean} true if the call to grocy succeed.
+ *                          @param products {[Integer]}  Array with ids for the sub recipes.
+ */
 function getNestedRecipes(recipe_external_id, callback = console.log) {
     let httpReq = config.grocy.url + "/api/objects/recipes_nestings?query[]=recipe_id=" + recipe_external_id + "&GROCY-API-KEY=" + config.grocy.apiKey;
     fetch(httpReq)
@@ -42,56 +64,93 @@ function getNestedRecipes(recipe_external_id, callback = console.log) {
         );
 }
 
-function getAllProducts(recipe_external_id, callback = console.log) {
-    getNestedRecipes(recipe_external_id, (status, recipeIds) => {
-        if (status == true) {
+/**
+ * Get all products for a recepe (including subrecipes)
+ * @param {Integer} recipe_external_id 
+ * @param {function} callback - function(status,products)
+ *                          @param status {boolean} true if the call to grocy succeed.
+ *                          @param products {[{product_id,amount}]} array with productid and the amount of the product needed for the recipe
 
-            recipeIds.push(recipe_external_id);
-            getProductIdsForRecipies(recipeIds, [], callback);
+ */
+function getRecipieProducts(recipe_external_id, callback = console.log) {
+    let httpReq = config.grocy.url + "/api/objects/recipes/" + recipe_external_id + "?GROCY-API-KEY=" + config.grocy.apiKey;
+    fetch(httpReq)
+        .then(res => res.json())
+        .then(
+            json => {
+                let recipeInfo = json; //RecipeInfo contains number of servings per recipe
+                getProductIds(recipe_external_id, (status, products) => { //get all products for the recipe
+                    if (status) {
+                        products.forEach(e => { e.amount = e.amount / (recipeInfo.base_servings * 1.0); });
+                        getNestedRecipes(recipe_external_id, (status, incRecepies) => {
+                            if (status) {
+                                getMultipleRecipieProducts(incRecepies, (status, moreProducts) => {
+                                    if (status) {
+                                        moreProducts.forEach(e => { e.amount = e.amount / (recipeInfo.base_servings * 1.0); });
+                                        callback(true, products.concat(moreProducts));
+                                    } else {
+                                        callback(false, moreProducts);
+                                    }
+                                })
+                            } else {
+                                callback(false, incRecepies);
+                            }
+                        })
+                    } else {
+                        callback(false, products);
+                    }
+
+                });
+
+            },
+            err => callback(false, err)
+        );
 
 
-        }
-    });
+
+
 }
 
+/**
+ * Get all products for an array of recipes
+ * @param {[Integer]} recipeIds 
+ * @param {function} callback - function(status,products)
+ *                          @param status {boolean} true if the call to grocy succeed.
+ *                          @param products {[{product_id,amount}]} array with productid and the amount of the product needed for the recipe
 
-
-function getProductIdsForRecipies(recipeIds, result, callback = console.log) {
-    if (recipeIds.length == 0) {
-        return callback(result);
-    } else {
-        getProductIds(recipeIds[0], (status, products) => {
-            getProductIdsForRecipies(recipeIds.slice(1), result.concat(products), callback)
-        })
-
-    }
-
-}
-
-function consumeProducts(quantity, products, result, callback = console.log) {
-    console.log("consumeProducts", quantity, products, result);
-    if (products.length == 0) {
-        callback(result);
+ * @param {[Integer]?} rest just a helper argument when recursing through all recipies 
+ * @returns 
+ */
+function getMultipleRecipieProducts(recipeIds, callback = console.log, rest = undefined) {
+    if (rest === undefined) {
+        getMultipleRecipieProducts(recipeIds.slice(0, 1), callback, recipeIds.slice(1));
         return;
     }
-    let p = products[0];
-    consumeProduct(p.product_id, quantity * parseFloat(p.amount), (status, actual_amount) => {
-        p.actual_amount = actual_amount;
-        consumeProducts(quantity, products.slice(1), result.concat(p), callback);
+    if (recipeIds.length === 0) {
+        callback(true, []);
+        return;
+    }
+
+    getRecipieProducts(recipeIds[0], (status, products) => {
+        getMultipleRecipieProducts(rest.slice(0, 1), (status, moreProducts) => {
+            callback(true, products.concat(moreProducts));
+
+        }, rest.slice(1));
+
     })
+
 
 }
 
-function consumeItem2(quantity, recipe_external_id,callback=console.log) {
-    getAllProducts(recipe_external_id, (products) => {
-        //console.log("consumeItem2",products);
-        consumeProducts(quantity,products,[],callback);
-    })
-}
-//consumeItem2(2,29);
+
+
+
+
+
+
 
 function consumeItem(quantity, recipe_external_id) {
-    getAllProducts(recipe_external_id, (products) => {
+    getRecipieProducts(recipe_external_id, (status, products) => {
         products.forEach(p=>{
             console.log("consumeItem",recipe_external_id,p);
             consumeProduct(p.product_id,quantity*parseFloat(p.amount),(status,actual_amount)=>{
@@ -106,6 +165,8 @@ function consumeItem(quantity, recipe_external_id) {
         })
     })
 }
+
+
 
 
 /**
@@ -124,7 +185,6 @@ function consumeProduct(productId, quantityAmount, callback = console.log) {
         .then(res => res.json())
         .then(
             json => {
-
                 let stock_amount = json.stock_amount;
                 let consumed_amount = quantityAmount < stock_amount ? quantityAmount : stock_amount;
                 httpReq = config.grocy.url + "/api/stock/products/" + productId + "/consume" + "?GROCY-API-KEY=" + config.grocy.apiKey;
@@ -155,7 +215,11 @@ function consumeProduct(productId, quantityAmount, callback = console.log) {
         );
 }
 
-
+/**
+ * Add a product to shopping list in Grocy
+ * @param {*} productId 
+ * @param {*} callback 
+ */
 function addToShoppingList(productId,callback=console.log) {
     let httpReq = config.grocy.url + "/api/stock/shoppinglist/add-product" + "?GROCY-API-KEY=" + config.grocy.apiKey;
     let body = {
