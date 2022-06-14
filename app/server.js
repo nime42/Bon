26e24d49d7a976d3849973f6b6626b568b0c6818
nime42 <nilsmeinhard@gmail.com>
2022-06-14 20:26:05 +0200

    try to avoidthat anyone from outside shutdown the server

--- app/server.js
+++ app/server.js
@@ -92,7 +92,7 @@ app.use((req,res,next)=>{
 
 
 app.get("/shutdown",(req,res) => {
-    var isLocal = (req.connection.localAddress === req.connection.remoteAddress);
+    var isLocal = ((req.socket.remoteAddress.match(/.*127.0.0.1/))); //not sure this always work when going through nginx
     if(isLocal) {
         loginHandler.saveSessions();
         console.log("Shutting down!");
