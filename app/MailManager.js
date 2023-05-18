f4f0c025e0f69a38c0e9829964be72126c471f0d
nime42 <nilsmeinhard@gmail.com>
2023-05-18 11:44:34 +0200

    mark sent mails as read when you fetch them

--- app/MailManager.js
+++ app/MailManager.js
@@ -3,43 +3,40 @@ const { simpleParser } = require("mailparser");
 const { mail } = require("../resources/config.js");
 const bonUtils = require("./BonUtils.js");
 
-
 var config = require("../resources/config.js");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = config.mailManager.rejectUnauthorized !== undefined ? config.mailManager.rejectUnauthorized : 1;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED =
+  config.mailManager.rejectUnauthorized !== undefined
+    ? config.mailManager.rejectUnauthorized
+    : 1;
 
 if (!config.mailManager.keepAlive) {
   config.mailManager.keepAlive = false;
 }
 
-
-
 function getMails(mailbox, searchCriteria, markAsRead, callback = console.log) {
   try {
     let allMessages = [];
     const imap = new Imap(config.mailManager);
-    imap.once('ready', () => {
+    imap.once("ready", () => {
       imap.openBox(mailbox, false, () => {
         imap.search(searchCriteria, (err, results) => {
           let numberOfMessagesLeft = results.length;
           if (results.length > 0) {
-            const f = imap.fetch(results, { bodies: '' });
-            f.on('message', msg => {
+            const f = imap.fetch(results, { bodies: "" });
+            f.on("message", (msg) => {
               let messageInfo = {};
-              msg.on('body', stream => {
+              msg.on("body", (stream) => {
                 messageInfo.stream = stream;
               });
-              msg.once('attributes', attrs => {
+              msg.once("attributes", (attrs) => {
                 messageInfo.attrs = attrs;
                 if (markAsRead) {
                   const { uid } = attrs;
-                  imap.addFlags(uid, ['\\Seen'], () => {
-                  });
+                  imap.addFlags(uid, ["\\Seen"], () => {});
                 }
-
               });
-              msg.once('end', () => {
-
+              msg.once("end", () => {
                 simpleParser(messageInfo.stream, async (err, parsed) => {
                   let minfo = {
                     date: parsed.date,
@@ -49,50 +46,53 @@ function getMails(mailbox, searchCriteria, markAsRead, callback = console.log) {
                     message: parsed.text,
                     //attachments: parsed.attachments,
                     messageId: parsed.messageId,
-                    unread: true
+                    unread: true,
                   };
-                  if (messageInfo?.attrs?.flags?.find(e => (e === '\\Seen'))) {
-                    minfo.unread = false;
+                  if (parsed.subject?.startsWith("SENT:")) {
+                    imap.addFlags(messageInfo?.attrs.uid, ["\\Seen"], () => {});
+                  } else {
+                    if (
+                      messageInfo?.attrs?.flags?.find((e) => e === "\\Seen")
+                    ) {
+                      minfo.unread = false;
+                    }
                   }
                   allMessages.push(minfo);
                   numberOfMessagesLeft--;
                   if (numberOfMessagesLeft === 0) {
+                    //console.log("nr of messages:" + allMessages.length);
                     callback(true, allMessages);
                   }
-                })
+                });
               });
             });
-            f.once('error', ex => {
+            f.once("error", (ex) => {
               callback(false, ex);
               return Promise.reject(ex);
             });
-            f.once('end', () => {
+            f.once("end", () => {
               imap.end();
             });
           } else {
             imap.end();
-            callback(true,allMessages);
+            //console.log("nr of messages:" + allMessages.length);
+            callback(true, allMessages);
           }
         });
       });
     });
 
-    imap.once('error', err => {
+    imap.once("error", (err) => {
       console.log(err);
       imap.end();
     });
 
-
     imap.connect();
   } catch (ex) {
     callback(false, ex);
-    console.log('getMails', ex);
+    console.log("getMails", ex);
   }
-};
-
-
-
-
+}
 
 function removeQuotedText(message) {
   return message.replace(/(^.*<.*@.*>.*$)*\n{0,2}(^>.*$)/gm, "").trim();
@@ -100,18 +100,22 @@ function removeQuotedText(message) {
 
 function getBonMails(prefix, id, markAsRead, callback = console.log) {
   let searchSubject = "#Bon:" + prefix + "-" + id + ":";
-  getMails("INBOX", ['ALL', ['SUBJECT', searchSubject]], markAsRead, (status, data) => {
-    if (status) {
-      let res = data.sort((a, b) => (a.date - b.date));
-      res.forEach(e => {
-        e.message = removeQuotedText(e.message);
-      });
-      callback(status, res);
-    } else {
-      callback(status, data);
-
+  getMails(
+    "INBOX",
+    ["ALL", ["SUBJECT", searchSubject]],
+    markAsRead,
+    (status, data) => {
+      if (status) {
+        let res = data.sort((a, b) => a.date - b.date);
+        res.forEach((e) => {
+          e.message = removeQuotedText(e.message);
+        });
+        callback(status, res);
+      } else {
+        callback(status, data);
+      }
     }
-  });
+  );
 }
 
 function getUnseenMails(prefix, callback = console.log) {
@@ -121,12 +125,17 @@ function getUnseenMails(prefix, callback = console.log) {
   } else {
     searchSubject = "#Bon:" + prefix + "-";
   }
-  getMails("INBOX", ['UNSEEN', ['SUBJECT', searchSubject]], false, (status, data) => {
-    callback(status, data);
-  });
+  getMails(
+    "INBOX",
+    ["UNSEEN", ["SUBJECT", searchSubject]],
+    false,
+    (status, data) => {
+      callback(status, data);
+    }
+  );
 }
 
-function getBonWithMails(prefix,mailsSince, callBack = console.log) {
+function getBonWithMails(prefix, mailsSince, callBack = console.log) {
   let searchSubject;
   if (prefix === "*") {
     searchSubject = "#Bon:";
@@ -134,23 +143,23 @@ function getBonWithMails(prefix,mailsSince, callBack = console.log) {
     searchSubject = "#Bon:" + prefix + "-";
   }
 
-  let searchCriteria=['ALL', ['SUBJECT', searchSubject]];
-  if(mailsSince) {
-    searchCriteria.push(["OR", ['SINCE', formatSearchDate(mailsSince)],"UNSEEN"])
+  let searchCriteria = ["ALL", ["SUBJECT", searchSubject]];
+  if (mailsSince) {
+    searchCriteria.push(["OR", ["SINCE", mailsSince], "UNSEEN"]);
   }
 
   getMails("INBOX", searchCriteria, false, (status, data) => {
-    incomingMails = data.filter((m) => (!m.subject.startsWith("SENT")));
+    incomingMails = data.filter((m) => !m.subject.startsWith("SENT"));
     let mails = {};
-    incomingMails.forEach(m => {
+    incomingMails.forEach((m) => {
       const b = getBonId(m.subject);
       const key = b.prefix + "-" + b.bonId;
       if (!mails[key]) {
         mails[key] = {
           prefix: b.prefix,
           bonId: b.bonId,
-          mail: m
-        }
+          mail: m,
+        };
       } else {
         if (mails[key].mail.date < m.date) {
           mails[key].mail = m;
@@ -158,13 +167,12 @@ function getBonWithMails(prefix,mailsSince, callBack = console.log) {
       }
     });
 
-    let res = Object.values(mails).sort((a, b) => (b.mail.date.getTime() - a.mail.date.getTime()));
+    let res = Object.values(mails).sort(
+      (a, b) => b.mail.date.getTime() - a.mail.date.getTime()
+    );
     callBack(status, res);
   });
-
-
 }
-
 
 function getBonId(subject) {
   let res = {};
@@ -177,8 +185,8 @@ function getBonId(subject) {
 }
 
 function getBonIds(mails, withPrefix) {
-  let res = {}
-  mails.forEach(m => {
+  let res = {};
+  mails.forEach((m) => {
     if (!m.subject.startsWith("SENT")) {
       const bon = getBonId(m.subject);
       if (bon.bonId != undefined) {
@@ -207,21 +215,30 @@ let incomingMailHelper = {
     kitchen_selects: "Ristet Rug vælger",
     company_name: "Firmanavn",
     invoice_info: "EAN/faktura info",
-    customer_info: "Dine ønsker"
-  }
-}
+    customer_info: "Dine ønsker",
+  },
+};
 
 function getIncomingOrders(subjectContains, callback) {
-  getMails("INBOX", ['UNSEEN', ['SUBJECT', subjectContains]], true, (status, data) => {
-    if (status) {
-      let bons = data.map(m => ({ orgMessage: m.message, bon: buildBon(parseIncomingMessage(m.message, incomingMailHelper.bonAttribMap)) }));
-      callback(true, bons);
-    } else {
-      callback(false, data);
+  getMails(
+    "INBOX",
+    ["UNSEEN", ["SUBJECT", subjectContains]],
+    true,
+    (status, data) => {
+      if (status) {
+        let bons = data.map((m) => ({
+          orgMessage: m.message,
+          bon: buildBon(
+            parseIncomingMessage(m.message, incomingMailHelper.bonAttribMap)
+          ),
+        }));
+        callback(true, bons);
+      } else {
+        callback(false, data);
+      }
     }
-  })
+  );
 }
-
 
 function buildBon(entries) {
   let bon = bonUtils.getEmptyBon();
@@ -244,16 +261,17 @@ function buildBon(entries) {
     let street_nr = bon.delivery_address.street_name.match(/(\d)+ ?[A-Z]?/);
     if (street_nr) {
       bon.delivery_address.street_nr = street_nr[0];
-      bon.delivery_address.street_name = bon.delivery_address.street_name.replace(bon.delivery_address.street_nr, "").trim();
+      bon.delivery_address.street_name = bon.delivery_address.street_name
+        .replace(bon.delivery_address.street_nr, "")
+        .trim();
     }
-  } catch (err) { }
+  } catch (err) {}
   try {
     let ean_nr = bon.invoice_info.match(/\d{13}/);
     if (ean_nr) {
       bon.customer.company.ean_nr = ean_nr[0];
     }
-  } catch (err) { }
-
+  } catch (err) {}
 
   if (bon.delivery_date === undefined) {
     bon.kitchen_info = "BEMÆRK, dato kunne ikke læses. Tjek email.";
@@ -261,22 +279,16 @@ function buildBon(entries) {
   return bon;
 }
 
-
-
-
-
-
-
 function parseDeliveryDate(entries) {
   let date = entries["delivery_date"];
   let time = entries["delivery_time"];
   let dateValue = undefined;
   if (date && date.match(/.* \d{1,2},\d{2,4}/)) {
-    dateValue = new Date(date) + 1; //need to add one day if date is on format "Month day, Year" 
+    dateValue = new Date(date) + 1; //need to add one day if date is on format "Month day, Year"
   } else {
     try {
       dateValue = new Date(date);
-    } catch (err) { }
+    } catch (err) {}
   }
   if (dateValue === undefined) {
     return undefined;
@@ -294,7 +306,6 @@ function parseDeliveryDate(entries) {
   }
 
   return dateValue.toISOString();
-
 }
 
 function getFromEntry(entries, attr) {
@@ -302,12 +313,13 @@ function getFromEntry(entries, attr) {
   return val !== undefined ? val : "";
 }
 
-
 function parseIncomingMessage(mess, entries) {
-  let res = {}
+  let res = {};
   mess = mess.replace(/CONTACT\n.*/, ""); //Not sure about this.
-  Object.keys(entries).forEach(k => {
-    let regExp = new RegExp(`(?<attr>${entries[k]}):\\n(?<value>[\\s\\S]*?)(?<end>(.*?:\\n|$))`);
+  Object.keys(entries).forEach((k) => {
+    let regExp = new RegExp(
+      `(?<attr>${entries[k]}):\\n(?<value>[\\s\\S]*?)(?<end>(.*?:\\n|$))`
+    );
     let m = mess.match(regExp);
     if (m) {
       res[k] = m.groups["value"];
@@ -315,51 +327,55 @@ function parseIncomingMessage(mess, entries) {
     } else {
       res[k] = undefined;
     }
-
-  })
+  });
   return res;
 }
 
-
 /**
  * Get the time difference between a local date and another timezone.
- * 
+ *
  * Could be usefull if you get date as a string without timezone but you know what timezone it come from
  * let d=new Date(dateString);
  * d.setTime(d-Helper.getLocalTimeOffsetDiff(d,"Europe/Copenhagen"));
- * @param {Date} date 
+ * @param {Date} date
  * @param {String} timeZone - A TZ database name (see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
  * @returns {Integer} difference in milliseconds
  */
 function getLocalTimeOffsetDiff(date, timeZone) {
   let dateWithoutSec = new Date(date);
   dateWithoutSec.setSeconds(0, 0);
-  let local = new Date(dateWithoutSec.toLocaleString("default", { timeZone: timeZone }));
+  let local = new Date(
+    dateWithoutSec.toLocaleString("default", { timeZone: timeZone })
+  );
   local.setSeconds(0, 0);
   return local - dateWithoutSec;
-
-
 }
 
 /**
  * returns date in the format DD-month-YYYY
- * @param {*} date 
- * @returns 
+ * @param {*} date
+ * @returns
  */
 function formatSearchDate(date) {
-  if(!date) {
+  if (!date) {
     return date;
   } else {
     //locale en-GB adds space between components
-    return date.toLocaleDateString('en-GB',{timeZone:"UTC",month:"short", day:"2-digit", year:"numeric"}).replaceAll(" ","-");
+    return date
+      .toLocaleDateString("en-GB", {
+        timeZone: "UTC",
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      })
+      .replaceAll(" ", "-");
   }
 }
-
 
 module.exports = {
   getBonMails: getBonMails,
   getUnseenMails: getUnseenMails,
   getBonIds: getBonIds,
   getIncomingOrders: getIncomingOrders,
-  getBonWithMails: getBonWithMails
+  getBonWithMails: getBonWithMails,
 };
