2cca46c91a0838a88f76f167ed06d99c1310eb3a
“nilsmeinhard” <nilsmeinhard@gmail.com>
2024-12-07 16:51:51 +0100

    get geoinfo for all bons

--- app/utils/syncGeoInfo.js
+++ app/utils/syncGeoInfo.js
@@ -7,8 +7,20 @@ var DB=new DBClass('./resources/bon.db');
 
 DB.useAddressLookUp(AddressSearch);
 
-const sql="select * from bons where delivery_date >date('now')";
-const bonIds=DB.getDbHandler().prepare(sql).all().map(r=>(r.id));
-bonIds.forEach(b=>{
-    DB.addGeoInfo(b);
-})
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function syncGeoInfo() {
+    const sql="select * from bons";
+    const bonIds=DB.getDbHandler().prepare(sql).all().map(r=>(r.id));
+    for (let i = 0; i < bonIds.length; i++) {
+        let b=bonIds[i];
+        console.log("adding geoinfo for bon "+b);
+        await sleep(2000);
+        DB.addGeoInfo(b);
+
+    }   
+  }
+
+syncGeoInfo();
